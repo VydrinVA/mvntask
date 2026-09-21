3a233c892cbd125a8ab80b33ea284173c8db8505
VydrinVA <vydrinwork@gmail.com>
2026-09-21 16:55:24 +0700

    Changed dependency from geometryapp to twodimensional

--- geometryapp/src/main/java/ru/vydrin/work/App.java
+++ geometryapp/src/main/java/ru/vydrin/work/App.java
@@ -12,26 +12,42 @@ public class App
 {
     public static void main( String[] args )
     {
+        System.out.println("Test Circle");
         createAndShowCircle();
+        System.out.println("Test Rectangle");
         createAndShowRectangle();
+        System.out.println("Test Triangle");
         createAndShowTriangle();
+        System.out.println("Test GeometryUtils");
+        testGeometryUtils();
     }
 
     private static void createAndShowTriangle() {
         Triangle triangle = new Triangle(3, 4, 5);
+        System.out.println(triangle);
         System.out.println(triangle.getArea());
         System.out.println(triangle.getPerimeter());
     }
 
     private static void createAndShowCircle() {
         Circle circle = new Circle(5);
+        System.out.println(circle);
         System.out.println(circle.getArea());
         System.out.println(circle.getPerimeter());
     }
 
     private static void createAndShowRectangle() {
         Rectangle rectangle = new Rectangle(5, 10);
+        System.out.println(rectangle);
         System.out.println(rectangle.getArea());
         System.out.println(rectangle.getPerimeter());
+    }
+
+    private static void testGeometryUtils() {
+        Rectangle rectangle = new Rectangle(15, 15);
+        Circle circle = new Circle(15);
+        System.out.println(rectangle);
+        System.out.println(circle);
+        System.out.println(GeometryUtils.largerByArea(rectangle, circle) + " is larger by Area");
     }
 }
